97027c6efac0b61b1d8bf40bad0ea4fb2fe6991f
benCoomes <bcoomes@g.clemson.edu>
2019-03-15 10:13:51 -0400

    Simple arguments to console app

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,13 +1,54 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using StarWarsQL.SchemaFirst;
 
 namespace StarWarsQLConsole
 {
     class Program
     {
+        static Dictionary<string, string> NamedQueries = new Dictionary<string, string>()
+        {
+            {"darthVadar", "{ person(id: \"4\") { name gender height skinColor hairColor homeworld vehicles} }"},
+            {"allPeople", "{ allPersons { name gender height skinColor hairColor homeworld vehicles } }"}
+        };
+
+        private static string ResolveQuery(string[] args)
+        {
+            string query = null;
+            
+            if(args.Length >= 1)
+            {
+                var queryArg = args[0];
+                if(queryArg.StartsWith('{'))
+                {
+                    query = queryArg;
+                }
+                else
+                {
+                    bool wasFound = NamedQueries.TryGetValue(queryArg, out var namedQuery);
+                    if(wasFound)
+                    {
+                        query = namedQuery;
+                    }
+                }
+            }
+            
+            return query;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine(StarWarsSchema.Execute("{ person(id: \"4\") { name gender height skinColor hairColor homeworld vehicles} }"));
+            var query = ResolveQuery(args);
+           
+            if(string.IsNullOrWhiteSpace(query))
+            {
+                Console.WriteLine("No query argument provided, or failed to parse query. Provide either a query or the named of a stored query. Stored queries: \n");
+                foreach(var name in NamedQueries.Keys) Console.WriteLine(name);
+                return;
+            }
+
+            Console.WriteLine(StarWarsSchema.Execute(query));
         }
     }
 }
