58452e45d5d0a9cd10e3bec2547cfabcbda4b96f
benCoomes <bcoomes@g.clemson.edu>
2019-03-15 10:13:26 -0400

    Add films to schema first

--- StarWarsQL/SchemaFirst/StarWarsSchema.cs
+++ StarWarsQL/SchemaFirst/StarWarsSchema.cs
@@ -29,52 +29,100 @@ namespace StarWarsQL.SchemaFirst
 				vehicles: [String]
 			}
 
+			type Film {
+				episodeID: Int
+				openingCrawl: String
+				releaseDate: String
+				title: String
+				producer: String
+				director: String
+				starships: [String]
+				species: [String]
+				planets: [String]
+				characters: [String]
+				vehicles: [String]
+			}
+
 			type Query {
-				allPersons: [Person]
 				person(id: String): Person
+				allPersons: [Person]
+				film(id: String): Film
+				allFilms: [Film]
 			}
 		", sb => {
 			sb.Types.Include<Query>();
 		});
 
-		public static string Execute(string query) => _schema.Execute(eo => { eo.Query = query; });
+		public static string Execute(string query) => _schema.Execute(eo => 
+		{ 
+			eo.Query = query; 
+			eo.ExposeExceptions = true; 
+		});
 
 		private class Query
 		{
 			private static HttpClient client = new HttpClient();
 
 			[GraphQLMetadata("person")]
-			public async Task<Person> GetPersonByID(string id){
-				using (var response = await client.GetAsync($"https://swapi.co/api/people/{id}/"))
-				{
-					if(response.IsSuccessStatusCode)
-					{
-						var json = await response.Content.ReadAsStringAsync();
-						return JsonConvert.DeserializeObject<Person>(json);
-					}
-					else
-					{
-						return null;
-					}
-				}
+			public Task<Person> GetPersonByID(string id){
+				return GetResourceByID<Person>("people", id);
 			}
 
 			[GraphQLMetadata("allPersons")]
-			public async Task<IEnumerable<Person>> AllPersons()
+			public Task<IEnumerable<Person>> AllPersons()
 			{
-				using(var response = await client.GetAsync("https://swapi.co/api/people/"))
+				return GetAll<Person>("people");
+			}
+
+			[GraphQLMetadata("film")]
+			public Task<Film> GetFilmByID(string id)
+			{
+				return GetResourceByID<Film>("films", id);
+			}
+
+			[GraphQLMetadata("allFilms")]
+			public Task<IEnumerable<Film>> AllFilms()
+			{
+				return GetAll<Film>("films");
+			}
+
+			private async Task<T> GetResourceByID<T>(string resourceName, string id)
+			{
+				using(var response = await client.GetAsync($"https://swapi.co/api/{resourceName}/{id}"))
 				{
-					if(response.IsSuccessStatusCode)
-					{
-						var json = await response.Content.ReadAsStringAsync();
-						var jobject = JsonConvert.DeserializeAnonymousType(json, new {Results = new List<Person>()});
-						return jobject.Results;
-					}
-					else
-					{
-						return new List<Person>();
-					}
+					response.EnsureSuccessStatusCode();
+					
+					var json = await response.Content.ReadAsStringAsync();
+					return JsonConvert.DeserializeObject<T>(json);
 				}
+			}
+
+			private async Task<IEnumerable<T>> GetAll<T>(string resourceName)
+			{
+				var results = new List<T>();
+				var next = $"https://swapi.co/api/{resourceName}";
+
+				do
+				{
+					
+					using(var response = await client.GetAsync(next))
+					{
+						response.EnsureSuccessStatusCode();
+
+						var json = await response.Content.ReadAsStringAsync();
+						var jobject = JsonConvert.DeserializeAnonymousType(json, new 
+						{
+							Results = new List<T>(),
+							Next = ""
+						});
+						
+						results.AddRange(jobject.Results);
+						next = jobject.Next;
+					}
+
+				} while( !String.IsNullOrWhiteSpace(next) );
+
+				return results;
 			}
 		}
 	}
---
+++ StarWarsQL/Types/Film.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace StarWarsQL.Types
+{
+    public class Film
+    {
+        [JsonProperty("episode_id")]
+        public int EpisodeID { get; set; }
+        
+        [JsonProperty("opening_crawl")]
+        public string OpeningCrawl { get; set; }
+
+        [JsonProperty("release_date")]
+        public string ReleaseDate { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("producer")]
+        public string Producer { get; set; }
+
+        [JsonProperty("director")]
+        public string Director { get; set; }
+
+        [JsonProperty("starships")]
+        public IEnumerable<string> Starships { get; set; }
+
+        [JsonProperty("species")]
+        public IEnumerable<string> Species { get; set; }
+
+        [JsonProperty("planets")]
+        public IEnumerable<string> Planets { get; set; }
+
+        [JsonProperty("characters")]
+        public IEnumerable<string> Characters { get; set; }
+
+        [JsonProperty("vehicles")]
+        public IEnumerable<string> Vehicles { get; set; }
+    }
+}
